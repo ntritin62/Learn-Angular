fe67a7a182fd36781bb218490c63aadab8f3c162
ntritin62 <21522677@gm.uit.edu.vn>
2024-01-22 17:13:46 +0700

    section-2-22

--- section-2/src/app/servers/servers.component.ts
+++ section-2/src/app/servers/servers.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  template: `
+    <app-server></app-server>
+    <app-server></app-server>`,
   styleUrl: './servers.component.css'
 })
-export class ServersComponent {
+export class ServersComponent implements OnInit{
+  constructor() {
+  }
+  ngOnInit() {
+  }
 
 }
