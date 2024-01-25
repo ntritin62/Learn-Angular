5ec9a18857f92baa81c9abd45583f24a432631f0
ntritin62 <21522677@gm.uit.edu.vn>
2024-01-25 18:06:46 +0700

    section-2-43

--- section-2/src/app/servers/servers.component.ts
+++ section-2/src/app/servers/servers.component.ts
@@ -10,6 +10,7 @@ export class ServersComponent implements OnInit{
   serverCreationStatus = "No server was created!"
   serverName = "TestServer";
   serverCreated = false;
+  servers = ['Testserver', 'Testserver 2']
   constructor() {
     setTimeout(()=> {this.allowNewServer=true},2000);
   }
@@ -18,6 +19,7 @@ export class ServersComponent implements OnInit{
 
   onCreateServer() {
     this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = "Server was created! Name is " + this.serverName;
   }
 
