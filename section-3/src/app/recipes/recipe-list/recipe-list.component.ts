ebd8d5f50a8c1566217c107537c8ec1a319ffdc8
ntritin62 <21522677@gm.uit.edu.vn>
2024-01-27 03:09:42 +0700

    section-3-56

--- section-3/src/app/recipes/recipe-list/recipe-list.component.ts
+++ section-3/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component,OnInit } from '@angular/core';
+import {Recipe} from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component,OnInit } from '@angular/core';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent implements OnInit{
-  recipes= [];
+  recipes: Recipe[]= [
+    new Recipe('A Test Recipe', 'A test', 'https://images.wsj.net/im-912950')
+  ];
 
   ngOnInit() {
   }
