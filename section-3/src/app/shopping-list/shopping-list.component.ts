69e48869b255a4040d0c30811e6389d82bfc3705
ntritin62 <21522677@gm.uit.edu.vn>
2024-01-27 22:41:08 +0700

    section-3-59

--- section-3/src/app/shopping-list/shopping-list.component.ts
+++ section-3/src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrl: './shopping-list.component.css'
 })
-export class ShoppingListComponent {
+export class ShoppingListComponent implements OnInit{
+
+  ngOnInit() {
+  }
 
 }
